bd9dddd44bf0020fabff4ee4f84f8a03608e99bb
chuanyue <chuanyue@me.com>
2016-02-10 20:51:41 +0800

    设定的功能全部完成

--- DrawingBoard/CanvasView.swift
+++ DrawingBoard/CanvasView.swift
@@ -16,12 +16,24 @@ class CanvasView: UIView {
     var pathColor:UIColor?
     var lineWidth:CGFloat?
     
+    @IBOutlet weak var photo:UIBarButtonItem!
+    
+    var image:UIImage?{didSet{
+        
+        setNeedsDisplay()
+        }
+    }
+    
     //MARK:- 初始化
     // 仅仅是加载xib的时候调用
     override func awakeFromNib() {
         //添加手势
         let pan = UIPanGestureRecognizer.init(target: self, action: "pan:")
         self.addGestureRecognizer(pan)
+        
+        lineWidth = 1
+        pathColor = UIColor.blackColor()
+        
     }
     
     // 当手指拖动的时候调用
@@ -41,35 +53,58 @@ class CanvasView: UIView {
         // 手指一直在拖动
         // 添加线到当前触摸点并设置相关属性
         if path != nil{
-            
-            path.addLineToPoint(curP)
-            
             //设置画笔宽
-            if lineWidth != nil{
-                path.lineWidth = lineWidth!
-            }
-            
+            path.lineWidth = lineWidth!
             //设置画笔颜色
-            if pathColor != nil{
-                path.pathColor = pathColor!
-            }
+            path.pathColor = pathColor!
+            //获取当前点
+            path.addLineToPoint(curP)
         }
     
         //重绘
         setNeedsDisplay()
     }
     
-    // 绘制图形
-    // 只要调用drawRect方法就会把之前的内容全部清空
+    //MARK:-  绘制图形
+    /**
+    注意：只要调用drawRect方法就会把之前的内容全部清空，所以之前画的内容需要保存
+    
+    - parameter rect: 当前canvasView
+    */
     override func drawRect(rect: CGRect) {
+        if image != nil{
+            
+            image!.drawInRect(rect)
+            
+        }
         
         if !paths.isEmpty {
             for path in paths{
+                
                 if path.pathColor != nil{
                     path.pathColor.set()
                 }
                 path.stroke()
             }
+            
         }
+    }
+    
+    //清屏
+    func clearScreen(){
+        paths.removeAll()
+        image = UIImage(named: "")
+        setNeedsDisplay()
+        photo.title = "照片"
+        
+    }
+    
+    //撤销
+    func undo(){
+        if paths.count != 0{
+            paths.removeLast()
+        }
+       
+        setNeedsDisplay()
     }
 }
--- DrawingBoard/ViewController.swift
+++ DrawingBoard/ViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate ,UINavigationControllerDelegate{
 
     @IBOutlet weak var canvasView: CanvasView!
+    @IBOutlet weak var slider: UISlider!
+    
+    var pathColors = [UIColor]()
+    var lineWidths = [CGFloat]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,95 @@ class ViewController: UIViewController {
     //选择画笔线宽
     @IBAction func lineWidth(sender: UISlider) {
         canvasView.lineWidth = CGFloat(sender.value)
+    }
+    
+    //清屏
+    @IBAction func clearScreen(sender: AnyObject) {
+        canvasView.clearScreen()
+    }
+    
+    //撤销
+    @IBAction func undo(sender: AnyObject) {
+        canvasView.undo()
+    }
+    
+    //MARK:- 擦除绘画
+    /**
+    只是利用画布的颜色重画，遮盖需涂改的部分
+    */
+    @IBAction func eraser(sender: AnyObject) {
+        //临时保存擦除前的画笔线宽和画笔颜色
+        pathColors.append(canvasView.pathColor!)
+        lineWidths.append(canvasView.lineWidth!)
+        
+        if canvasView.pathColor != UIColor.whiteColor(){
+            
+            canvasView.pathColor = UIColor.whiteColor()
+            
+        }else{
+            //取出保存的线宽和颜色进行绘画
+            canvasView.pathColor = pathColors[0]
+            canvasView.lineWidth = lineWidths[0]
+            //将线宽调整滑块复位到橡皮擦使用前的位置
+            slider.value = Float(lineWidths[0])
+            //删除临时保存的线宽颜色数据
+            pathColors.removeAll()
+            lineWidths.removeAll()
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        canvasView.image = image
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func pickerPhoto(sender: UIBarButtonItem) {
+        if sender.title == "照片"{
+            let pickerVc = UIImagePickerController()
+            pickerVc.sourceType = .SavedPhotosAlbum
+            
+            pickerVc.delegate = self
+            
+            presentViewController(pickerVc, animated: true, completion: nil)
+            sender.title = "删除照片"
+        }else if sender.title == "删除照片"{
+            sender.title = "照片"
+            canvasView.image = UIImage(named: "")
+        }
+        
+    }
+    
+    
+    @IBAction func save(sender: AnyObject) {
+        //开启上下文
+        UIGraphicsBeginImageContextWithOptions(canvasView.bounds.size, false, 0)
+        //获取当前上下文
+        let ctx = UIGraphicsGetCurrentContext()
+        //将图层渲染到上下文
+        canvasView.layer.renderInContext(ctx!)
+        //获取上下文图片
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        //关闭上下文
+        UIGraphicsEndImageContext()
+        
+        // 保存画板的内容放入相册
+        // image:写入的图片
+        // completionTarget图片保存监听者
+        // 注意：以后写入相册方法中，想要监听图片有没有保存完成，保存完成的方法不能随意乱写
+        UIImageWriteToSavedPhotosAlbum(image, self, "image:didFinishSavingWithError:contextInfo:", nil)
+        
+    }
+    
+    func image(image:UIImage,didFinishSavingWithError error:NSError?,contextInfo:AnyObject){
+        if error != nil{
+            print(error)
+        }else{
+            print("图片保存成功")
+
+        }
+    
     }
     
     override func didReceiveMemoryWarning() {
